359c0fa487661b53670084d2e4b7ee1774e020a8
zhanglong12 <zhanglong12@jd.com>
2020-05-21 15:30:13 +0800

    EventEmitter commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs';
 import cleanup from 'rollup-plugin-cleanup'
-import {uglify } from 'rollup-plugin-uglify'
+import {uglify} from 'rollup-plugin-uglify'
+import EventEmitter from 'eventemitter3'
 
 var env = process.env.NODE_ENV
 var config = {
@@ -19,12 +21,18 @@ var config = {
     babel({
       exclude: 'node_modules/**'
     }),
+    commonjs({
+      include: /node_modules/,
+      namedExports: {
+        'eventemitter3': Object.keys(EventEmitter),
+      }
+    }),
     replace({
       'process.env.NODE_ENV': JSON.stringify(env)
     }),
     cleanup()
   ],
-  external: ['react', 'react-dom', 'prop-types', 'eventemitter3'],
+  external: ['react', 'react-dom', 'prop-types'],
 }
 if (env === 'production') {
   config.plugins.push(uglify())
