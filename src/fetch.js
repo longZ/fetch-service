b2e73674f297a9976879e5c0a7dd0ec197d0d053
zhanglong12 <zhanglong12@jd.com>
2022-06-30 12:53:16 +0800

    0.2.11

--- src/fetch.js
+++ src/fetch.js
@@ -1,7 +1,6 @@
 import { fetch as fetchPolyfill } from 'whatwg-fetch'
-import {isFunction} from "./util";
 
 // 如果浏览器支持fetch则使用浏览器的fetch
-const fetch = isFunction(window.fetch) ? window.fetch : fetchPolyfill
+const fetch = fetchPolyfill
 
 export default fetch
